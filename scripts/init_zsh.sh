399505df93c546a71b8e1aa094f457f55ee9198d
tao-lintian <wojiaowenzhong233@outlook.com>
2025-07-16 17:56:33 +0800

    zsh history search

--- scripts/init_zsh.sh
+++ scripts/init_zsh.sh
@@ -2,7 +2,7 @@ export ZSH=$HOME/.oh-my-zsh
 
 ZSH_THEME="jonathan"
 
-plugins=(git zsh-autosuggestions zsh-syntax-highlighting z)
+plugins=(git zsh-autosuggestions zsh-syntax-highlighting z history-substring-search)
 
 ZSH_CUSTOM=$HOME/zsh_custom
 
@@ -14,4 +14,6 @@ source $ZSH/oh-my-zsh.sh
 #     config submodule update --init
 
 alias config='/usr/bin/git --git-dir=$HOME/.mycfg/ --work-tree=$HOME'
+bindkey "^[[A" history-substring-search-up
+bindkey "^[[B" history-substring-search-down
 
