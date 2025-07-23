aa34da25062bcac8ea055b2e1c2d90768c241080
tao-lintian <wojiaowenzhong233@outlook.com>
2025-07-23 14:19:33 +0800

    update documentation for initialization

--- scripts/init_zsh.sh
+++ scripts/init_zsh.sh
@@ -9,9 +9,14 @@ ZSH_CUSTOM=$HOME/zsh_custom
 source $ZSH/oh-my-zsh.sh
 
 # at $HOME, execute:
+#     git clone --bare https://github.com/arusuki/.mycfg.git .mycfg
+#     alias config='/usr/bin/git --git-dir=$HOME/.mycfg/ --work-tree=$HOME'
 #     config config --local status.showUntrackedFiles no
 #     config checkout
 #     config submodule update --init
+#
+#  to initialize tmux tpm plugins, type:
+#     <C-D>I
 
 alias config='/usr/bin/git --git-dir=$HOME/.mycfg/ --work-tree=$HOME'
 bindkey "^[[A" history-substring-search-up
