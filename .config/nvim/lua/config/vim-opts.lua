bfc58dbd09a1b0266461e92799744d2db17beff1
tao-lintian <wojiaowenzhong233@outlook.com>
2025-11-06 16:30:20 +0800

    add tmux-vim navigation support and tmux window swap

--- .config/nvim/lua/config/vim-opts.lua
+++ .config/nvim/lua/config/vim-opts.lua
@@ -7,10 +7,10 @@ vim.go.showtabline = 0
 vim.opt.swapfile = false
 
 -- Navigate vim panes better
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+-- vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+-- vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+-- vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+-- vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
 vim.keymap.set('n', 'X', ":resize +5<CR>")
 vim.keymap.set('n', 'S', ":resize -5<CR>")
