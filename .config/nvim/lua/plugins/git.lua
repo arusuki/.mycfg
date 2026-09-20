e3013f92695a422956033bd4e672bd0435c8e55f
tao-lintian <wojiaowenzhong233@outlook.com>
2026-09-20 14:22:58 +0800

    feat(nvim): browse Git hunks through Telescope

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -6,6 +6,32 @@ return {
     "lewis6991/gitsigns.nvim",
     config = function()
       require("gitsigns").setup({current_line_blame_opts = {delay = 500}})
+      vim.keymap.set('n', '<leader>gh', function()
+        require('gitsigns').setqflist(0, { open = false }, vim.schedule_wrap(function(err)
+          if err then
+            vim.notify(tostring(err), vim.log.levels.ERROR)
+            return
+          end
+          if vim.fn.getqflist({ size = 0 }).size == 0 then
+            vim.notify('当前文件没有可显示的 Git hunks', vim.log.levels.INFO)
+            return
+          end
+          require('telescope.builtin').quickfix({ prompt_title = 'Current File Git Hunks' })
+        end))
+      end, { desc = 'Browse current file Git hunks' })
+      vim.keymap.set('n', '<leader>gH', function()
+        require('gitsigns').setqflist('all', { open = false }, vim.schedule_wrap(function(err)
+          if err then
+            vim.notify(tostring(err), vim.log.levels.ERROR)
+            return
+          end
+          if vim.fn.getqflist({ size = 0 }).size == 0 then
+            vim.notify('当前工作区没有可显示的 Git hunks', vim.log.levels.INFO)
+            return
+          end
+          require('telescope.builtin').quickfix({ prompt_title = 'Workspace Git Hunks' })
+        end))
+      end, { desc = 'Browse workspace Git hunks' })
       vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
       vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
       vim.keymap.set('n', '<leader>gn', ':Gitsigns next_hunk<CR>')
